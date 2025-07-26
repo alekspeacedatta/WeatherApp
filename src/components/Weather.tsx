866b20bdec1edb8fb6061f08ee6dca756d29a774
aleksandre <faleqs33@gmail.com>
2025-07-26 10:18:05 +0400

    adding style

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -16,17 +16,27 @@ const Weather = () => {
 
     return (
         <>
-            <form>
-                <input type="text" value={city} placeholder="enter city name" onChange={e => setCity(e.target.value)} />
-                <button type="submit">Search</button>
-            </form>
             <div className="weather">
-                {/* ეს იმიტომ დავაკომენტარე რომ მაგ აიპია მისამართზე აღარ მქონდა წვდომა */}
-                {/* <h1>your current city: {weather.city}</h1> */}
+                <section>
+                    <h1>Weather App</h1>
+                    <p>Get Current weather and 5-day forecast</p>
+                </section>
+                <form>
+                    <div className="search-box">
+                        <i className="fa-solid fa-location-dot"></i>
+                        <input type="text" value={city} placeholder="enter city name" onChange={e => setCity(e.target.value)} />
+                    </div>
+                    <button type="submit">
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                        Search
+                    </button>
+                </form>
                 <h2>
                     Current temperature in your city: {fetchedWeather?.current?.temperature_2m ? `${fetchedWeather.current.temperature_2m}°C` : "No data"}
                 </h2>
             </div>
+                {/* ეს იმიტომ დავაკომენტარე რომ მაგ აიპია მისამართზე აღარ მქონდა წვდომა */}
+                {/* <h1>your current city: {weather.city}</h1> */}
         </>
     )
 }
