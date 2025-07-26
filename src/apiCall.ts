fe946f62f61405fa75039cb128a3496e916a291c
aleksandre <faleqs33@gmail.com>
2025-07-26 20:46:42 +0400

    adding API_KEY and BASE_URL in api call

--- src/apiCall.ts
+++ src/apiCall.ts
@@ -3,7 +3,7 @@ const API_KEY = '9762ca04c120054143401373586ca59d';
 
 export const weatherResponse = async ( cityName: string) => {
     try {
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=9762ca04c120054143401373586ca59d&units=metric`);
+        const res = await fetch(`https://${BASE_URL}/weather?q=${cityName}&APPID=${API_KEY}&units=metric`);
         if(!res.ok) throw new Error("Error: Weather Response Res is Not OK");
         const data = await res.json();
         return data;
